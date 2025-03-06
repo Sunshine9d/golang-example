d0ff1f7d2e584c387dd29e1aa2cd42a801b9f94c
do.nguyen <nvdo9941@gmail.com>
2025-03-06 13:59:32 +0700

    feat(cryptit): change files

---
+++ cryptit/go.mod
@@ -0,0 +1,3 @@
+module github.com/Sunshine9d/golang-example/cryptit
+
+go 1.23.2
--- cryptit/main.go
+++ cryptit/main.go
@@ -1,1 +1,13 @@
-package cryptit
+package main
+
+import (
+	"fmt"
+	"github.com/Sunshine9d/golang-example/cryptit/decrypt"
+	"github.com/Sunshine9d/golang-example/cryptit/encrypt"
+)
+
+func main() {
+	encryptedStr := encrypt.Nimbus("Kodekloud")
+	fmt.Println(encryptedStr)
+	fmt.Println(decrypt.Nimbus(encryptedStr))
+}
